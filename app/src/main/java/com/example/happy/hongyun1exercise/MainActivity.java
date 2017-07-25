6fab2d7445e0b0ac4469c25e7380afb04e12fd23
xiaocaimeng007 <771540581@qq.com>
2017-07-25 20:31:22 +0800

    初始化

--- app/src/main/java/com/example/happy/hongyun1exercise/MainActivity.java
+++ app/src/main/java/com/example/happy/hongyun1exercise/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.happy.hongyun1exercise;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.RadioGroup;
+
+import com.youth.banner.Banner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Banner mBannerMain;
+    private RecyclerView mRecyclerviewMain;
+    private RadioGroup mRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mBannerMain = (Banner) findViewById(R.id.banner_main);
+        mRecyclerviewMain = (RecyclerView) findViewById(R.id.recyclerview_main);
+        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
     }
 }
